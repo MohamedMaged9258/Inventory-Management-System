07c63f0bfaee25b657805d02f644ff1be19eeb7a
Mohamed Maged <mohamedmaged9258@gmail.com>
2023-12-01 16:41:48 +0200

    edit the main Println

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello not waleed");
+        System.out.println("Hello not salah");
         System.out.println("hello");
     }
 }
