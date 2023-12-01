b313b66ea3e45dafccd32c625020e38ad89c600e
Youssef ELBanna <youssefelbanna88@gmail.com>
2023-12-01 15:35:20 +0200

    I edited the println

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Komy");
+        System.out.println("Hello Walid,Komy,Maged");
+        System.out.println("Hiiiiii");
     }
 }
