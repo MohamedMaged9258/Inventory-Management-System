a9573fd738e02c3a90beea932ed8457e47fc89d4
Youssef ELBanna <youssefelbanna88@gmail.com>
2023-12-01 15:55:09 +0200

    I edited the 2nd println

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Walid,Komy,Maged");
-        System.out.println("halla");
+        System.out.println("changed the 2nd println");
     }
 }
