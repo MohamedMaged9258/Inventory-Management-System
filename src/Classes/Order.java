c14e3b8519aac8a5e46e1c9d3ae2d0b7814d8393
Mohamed Maged <mohamedmaged9258@gmail.com>
2023-12-01 16:47:11 +0200

    Clean Start

--- src/Classes/Order.java
+++ src/Classes/Order.java
@@ -1,7 +1,5 @@
 package Classes;
 
 public class Order {
-    public Order() {
-        System.out.println("Orders have been purchased");
-    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello not salah");
-        System.out.println("hello");
+        System.out.println("Hello Clean Start");
     }
 }
